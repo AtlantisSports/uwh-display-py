ad74cee6e948ed57727c33b839d906b4506c6a4f
Jonathan Roelofs <jonathan@codesourcery.com>
2018-01-22 11:31:39 -0700

    WIP: Matrix

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='uwh-display',
     version='2.0.0',
-    packages=find_packages(),
+    packages=find_packages('rgbmatrix'),
     scripts=['bin/uwhdd'],
 )
---
+++ uwhd/matrix_canvas_viewer.py
@@ -0,0 +1,27 @@
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
+from .canvas_viewer import CanvasViewer
+
+class MatrixCanvasViewer(CanvasViewer):
+    def __init__(self):
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.chain_length = 2
+        options.parallel = 1
+        options.hardware_mapping = 'regular'
+
+        self._matrix = RGBMatrix(options=options)
+
+        self._image = Image.new("RGB", (self._matrix.width,
+                                        self._matrix.height),
+                                "white")
+
+    def show(self, c):
+        self.w.delete(tk.ALL)
+
+        pix = self._image.load()
+        for y in range(c.h):
+            for x in range(c.w):
+                pix[x, y] = c.get(x, y)
+
+         self._matrix.SetImage(self._image)
