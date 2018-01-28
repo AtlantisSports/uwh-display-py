fc87a76590b4b6b37378eca2af395990f1ad8599
Jonathan Roelofs <jonathan@codesourcery.com>
2018-01-28 10:02:59 -0700

    Add ' ' to large fonts

--- uwhd/font.py
+++ uwhd/font.py
@@ -108,7 +108,8 @@ class Font(object):
             '7': 'fonts/11x20/ascii_7.ppm',
             '8': 'fonts/11x20/ascii_8.ppm',
             '9': 'fonts/11x20/ascii_9.ppm',
-            ':': 'fonts/11x20/ascii_COLON.ppm'
+            ':': 'fonts/11x20/ascii_COLON.ppm',
+            ' ': 'fonts/11x20/ascii_SPACE.ppm'
         }
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -132,7 +133,8 @@ class Font(object):
             '6': 'fonts/15x29/ascii_6.ppm',
             '7': 'fonts/15x29/ascii_7.ppm',
             '8': 'fonts/15x29/ascii_8.ppm',
-            '9': 'fonts/15x29/ascii_9.ppm'
+            '9': 'fonts/15x29/ascii_9.ppm',
+            ' ': 'fonts/15x29/ascii_SPACE.ppm'
         }
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
