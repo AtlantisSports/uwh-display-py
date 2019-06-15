56ee456ca875237befe36ea72db8f7ddcc911680
Jonathan Roelofs <jroelofs@jroelofs.com>
2019-06-15 11:31:25 -0600

    v2: add provisions for inverting

--- uwhd/matrix_canvas_viewer.py
+++ uwhd/matrix_canvas_viewer.py
@@ -3,7 +3,7 @@ from PIL import Image
 from .canvas_viewer import CanvasViewer
 
 class MatrixCanvasViewer(CanvasViewer):
-    def __init__(self):
+    def __init__(self, window=None):
         options = RGBMatrixOptions()
         options.rows = 32
         options.chain_length = 3
@@ -18,10 +18,23 @@ class MatrixCanvasViewer(CanvasViewer):
 
         self._offscreen = self._matrix.CreateFrameCanvas()
 
+        if window is None:
+            window = { 'w': c.w, 'h': c.h, 'x': 0, 'y': 0, 'invert': False }
+
+        self.window = window
+        self.invert = window['invert']
+
+        self.cw = window['w']
+        self.ch = window['h']
+
     def show(self, c):
-        for y in range(c.h):
-            for x in range(c.w):
-                color = c.get(x, y)
+        for y in range(self.ch):
+            for x in range(self.cw):
+                xo = self.cw - x if self.invert else x
+                yo = self.ch - y if self.invert else y
+
+                color = c.get(self.window['x'] + xo, self.window['y'] + yo)
                 self._offscreen.SetPixel(x, y, color.r, color.g, color.b)
 
         self._offscreen = self._matrix.SwapOnVSync(self._offscreen)
+
--- uwhd/tk_canvas_viewer.py
+++ uwhd/tk_canvas_viewer.py
@@ -8,14 +8,17 @@ class TkCanvasViewer(CanvasViewer):
         self.master = master
 
         if window is None:
-            window = { 'w': c.w, 'h': c.h, 'x': 0, 'y': 0 }
+            window = { 'w': c.w, 'h': c.h, 'x': 0, 'y': 0, 'invert': False }
 
         self.window = window
+        self.invert = window['invert']
 
         self.px_w = 5
         self.px_h = 5
-        self.canv_w = self.px_w * window['w']
-        self.canv_h = self.px_h * window['h']
+        self.cw = window['w']
+        self.ch = window['h']
+        self.canv_w = self.px_w * self.cw
+        self.canv_h = self.px_h * self.ch
         f = tk.Frame(master, width=self.canv_w, height=self.canv_h)
         f.pack()
         w = tk.Canvas(f, width=self.canv_w, height=self.canv_h)
@@ -24,6 +27,10 @@ class TkCanvasViewer(CanvasViewer):
 
 
     def show_px(self, x, y, color):
+        if self.invert:
+            x = self.cw - x
+            y = self.ch - y
+
         self.w.create_rectangle((x + 0) * self.px_w, (y + 0) * self.px_h,
                                 (x + 1) * self.px_w, (y + 1) * self.px_h,
                                 fill=color.as_hex())
@@ -31,8 +38,8 @@ class TkCanvasViewer(CanvasViewer):
     def show(self, c):
         self.w.delete(tk.ALL)
 
-        for y in range(self.window['h']):
-            for x in range(self.window['w']):
+        for y in range(self.ch):
+            for x in range(self.cw):
                 self.show_px(x, y,
                              c.get(self.window['x'] + x,
                                    self.window['y'] + y))
